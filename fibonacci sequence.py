b7967c2f56658d6131587a727e87a4e120ce5c4b
waivey <waivey@gmail.com>
2019-07-21 21:17:37 +0100

    updated fib sequence

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -4,10 +4,15 @@ print('Fibonacci Sequence Generator')
 
 n = int(input('Enter a number (max 15): '))
 
+sequence = [0, 1]
+
 a = 1
 b = 0
 while (a < n):
 	c = b
 	b = a
 	a = c + b
-	print(a)
+	sequence.append(a)
+
+print( sequence )
+
